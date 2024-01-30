577b616eb2b9bf109ed859299e249d3c96e4fd0c
axlmemote <daniil.ustimencko@gmail.com>
2024-01-30 17:36:37 +0300

    better

--- src/app/profile/layouts/profile-layout/profile-layout.component.ts
+++ src/app/profile/layouts/profile-layout/profile-layout.component.ts
@@ -29,10 +29,10 @@ export class ProfileLayoutComponent {
 
     {
       head: 'Blogging',
-      subhead: 'Post something',
+      subhead: 'Blog the world',
       avatar:
         'https://ilovemy.pet/cdn/shop/products/the-poet-retriever-poem-for-dog-lovers_grande.webp?v=1678081625',
-      description: `Blog the world`,
+      description: `You currently have 10 posts`,
       background:
         'https://img.freepik.com/premium-photo/serious-dog-writer-dog-journalist-dog-secretary-dog-with-book-feather_556412-5717.jpg',
       buttons: [
@@ -51,7 +51,7 @@ export class ProfileLayoutComponent {
       subhead: 'Table of users',
       avatar:
         'https://img.freepik.com/premium-photo/purebred-cool-little-poodles-with-curls-hairstyles-dark-background_124507-32575.jpg?size=626&ext=jpg&ga=GA1.1.1546980028.1702598400&semt=ais',
-      description: `See your people`,
+      description: `Your blog has 23 subscribers`,
       background:
         'https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/81pU32sgusL._AC_UF894,1000_QL80_.jpg',
       buttons: [
@@ -63,10 +63,10 @@ export class ProfileLayoutComponent {
     },
     {
       head: 'Topics',
-      subhead: 'Categorize your posts',
+      subhead: 'Topics of your blog',
       avatar:
         'https://m.media-amazon.com/images/I/81aDhGOSaFL._AC_UF894,1000_QL80_.jpg',
-      description: `Topics of your blog`,
+      description: `Your blog has 5 topics`,
       background:
         'https://img.freepik.com/premium-vector/dog-wearing-glasses-tshirt-design-animal-nature-concept-isolated_884500-21073.jpg',
       buttons: [
@@ -81,7 +81,7 @@ export class ProfileLayoutComponent {
       subhead: 'Photoes, videos, files',
       avatar:
         'https://static.vecteezy.com/system/resources/previews/023/184/670/large_2x/portrait-of-a-cute-corgi-dog-in-an-astronaut-suit-ai-generative-image-free-photo.jpg',
-      description: `Gallery of your photoes`,
+      description: `You have uploaded 12 files in total`,
       background:
         'https://i.etsystatic.com/39933715/r/il/e48ab6/4525689053/il_570xN.4525689053_d7cb.jpg',
       buttons: [
