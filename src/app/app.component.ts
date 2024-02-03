c0e5cb5cc05aa618ed8b053f5c5401f188df8944
skycader <skycader@yandex.ru>
2024-02-03 21:18:56 +0300

    main: theme storing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ConfigService } from './ardon-common/services/config.service';
 import { Title } from '@angular/platform-browser';
 import { ArdonConfigInterface } from './ardon-common/models/ardonConfig.interface';
+import { ThemeService } from './ardon-core/services/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -14,11 +15,14 @@ export class AppComponent {
   constructor(
     private config: ConfigService,
     private titleService: Title,
+    private themeService: ThemeService,
   ) { }
 
   ngOnInit() {
     this.config.config$.subscribe((config: ArdonConfigInterface) => {
       this.titleService.setTitle(config.appName);
     });
+
+    this.themeService.applyTheme();
   }
 }
--- src/app/ardon-core/services/theme.service.ts
+++ src/app/ardon-core/services/theme.service.ts
@@ -6,6 +6,8 @@ import { Injectable } from '@angular/core';
 export class ThemeService {
   private _isDarkTheme = false;
   public get isDarkTheme() {
+    const isDarkTheme = localStorage.getItem('ardon-theme');
+    if (isDarkTheme === 'true') this._isDarkTheme = true;
     return this._isDarkTheme;
   }
 
@@ -14,16 +16,22 @@ export class ThemeService {
   }
   constructor() { }
 
+  public applyTheme() {
+    this.isDarkTheme ? this.enableDarkTheme() : this.disableDarkTheme();
+  }
+
   public enableDarkTheme() {
     document.querySelector('body')!.classList.add('dark-theme');
     document.querySelector('body')!.setAttribute('data-bs-theme', 'dark');
-
+    localStorage.setItem('ardon-theme', 'true');
     this.isDarkTheme = true;
   }
 
   public disableDarkTheme() {
     document.querySelector('body')!.classList.remove('dark-theme');
     document.querySelector('body')!.setAttribute('data-bs-theme', 'light');
+    localStorage.removeItem('ardon-theme');
+
     this.isDarkTheme = false;
   }
 
