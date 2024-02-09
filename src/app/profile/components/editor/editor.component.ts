1314b363f8196499bfb9539c8677698c364ba844
axlmemote <daniil.ustimencko@gmail.com>
2024-02-09 11:32:45 +0300

    main: editor init

--- src/app/profile/components/editor/editor.component.ts
+++ src/app/profile/components/editor/editor.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'ardon-editor',
   templateUrl: './editor.component.html',
-  styleUrl: './editor.component.scss'
+  styleUrl: './editor.component.scss',
 })
 export class EditorComponent {
+  public preventDrag: boolean = true;
 
+  @HostBinding('class.grabbing') grabbing: boolean = false;
+
+  movies = [
+    'Episode I - The Phantom Menace',
+    'Episode II - Attack of the Clones',
+    'Episode III - Revenge of the Sith',
+    'Episode IV - A New Hope',
+    'Episode V - The Empire Strikes Back',
+    'Episode VI - Return of the Jedi',
+    'Episode VII - The Force Awakens',
+    'Episode VIII - The Last Jedi',
+    'Episode IX – The Rise of Skywalker',
+  ];
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
+  }
 }
--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,12 +1,13 @@
+import { CdkDrag, CdkDropList } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ArdonCommonModule } from '../ardon-common/ardon-common.module';
 import { MaterialModule } from '../material/material.module';
+import { EditorComponent } from './components/editor/editor.component';
+import { SignFormComponent } from './components/sign-form/sign-form.component';
 import { ProfileLayoutComponent } from './layouts/profile-layout/profile-layout.component';
 import { profileResolver } from './utils/resolvers/profile.resolver';
-import { SignFormComponent } from './components/sign-form/sign-form.component';
-import { EditorComponent } from './components/editor/editor.component';
 
 @NgModule({
   declarations: [ProfileLayoutComponent, SignFormComponent, EditorComponent],
@@ -14,6 +15,8 @@ import { EditorComponent } from './components/editor/editor.component';
     MaterialModule,
     ArdonCommonModule,
     CommonModule,
+    CdkDropList,
+    CdkDrag,
     RouterModule.forChild([
       {
         path: 'profile',
@@ -21,6 +24,13 @@ import { EditorComponent } from './components/editor/editor.component';
           profile: profileResolver,
         },
         component: ProfileLayoutComponent,
+      },
+      {
+        path: 'editor/:id',
+        resolve: {
+          profile: profileResolver,
+        },
+        component: EditorComponent,
       },
     ]),
   ],
