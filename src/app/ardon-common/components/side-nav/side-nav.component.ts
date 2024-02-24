424469f8960dc10c4492395253c03ffd69d227b1
skycader <skycader@yandex.ru>
2024-02-25 02:00:04 +0300

    dev: flexixble title + rus locale

--- src/app/ardon-common/components/side-nav/side-nav.component.ts
+++ src/app/ardon-common/components/side-nav/side-nav.component.ts
@@ -4,7 +4,6 @@ import { of, switchMap, timer } from 'rxjs';
 
 export interface Section {
   name: string;
-  updated: Date;
 }
 
 @Component({
@@ -15,26 +14,15 @@ export interface Section {
 export class SideNavComponent {
   folders: Section[] = [
     {
-      name: 'Photos',
-      updated: new Date('1/1/16'),
-    },
-    {
-      name: 'Recipes',
-      updated: new Date('1/17/16'),
-    },
-    {
-      name: 'Work',
-      updated: new Date('1/28/16'),
+      name: 'Услуги',
     },
   ];
   notes: Section[] = [
     {
-      name: 'Vacation Itinerary',
-      updated: new Date('2/20/16'),
+      name: 'Мощение дорожек и площадок',
     },
     {
-      name: 'Kitchen Remodel',
-      updated: new Date('1/18/16'),
+      name: 'Дренаж и система водоотведения',
     },
   ];
 
