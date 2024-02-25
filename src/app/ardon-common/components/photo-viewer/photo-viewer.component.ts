5155d2d79365318382a968565aa5ef63e394de46
skycader <skycader@yandex.ru>
2024-02-25 04:13:09 +0300

    dev: zooming okay

--- src/app/ardon-common/components/photo-viewer/photo-viewer.component.ts
+++ src/app/ardon-common/components/photo-viewer/photo-viewer.component.ts
@@ -1,16 +1,68 @@
-import { Component, HostListener, Input } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  Input,
+  ViewChild,
+} from '@angular/core';
 import { PhotoViewerService } from '../../services/photo-viewer.service';
 
+import Panzoom from '@panzoom/panzoom';
 @Component({
   selector: 'ardon-photo-viewer',
   templateUrl: './photo-viewer.component.html',
   styleUrl: './photo-viewer.component.scss',
 })
 export class PhotoViewerComponent {
+  public panzoom: any;
+  @ViewChild('world') world!: ElementRef;
+
+  public stopPropagation(event: any) {
+    event.stopPropagation();
+  }
+  zoom() {
+    this.panzoom.pan(0, 0, { animate: true });
+    this.panzoom.zoom(1.5, { animate: true });
+  }
+  initPanzoom() {
+    const elem: any = this.world.nativeElement;
+
+    const elementIsVisibleInViewport = (el: any, partiallyVisible = false) => {
+      const { top, left, bottom, right } = el.getBoundingClientRect();
+      const { innerHeight, innerWidth } = window;
+      return partiallyVisible
+        ? ((top > 0 && top < innerHeight) ||
+          (bottom > 0 && bottom < innerHeight)) &&
+        ((left > 0 && left < innerWidth) ||
+          (right > 0 && right < innerWidth))
+        : top >= 0 && left >= 0 && bottom <= innerHeight && right <= innerWidth;
+    };
+
+    this.panzoom = Panzoom(elem, {
+      maxScale: 100,
+      minScale: 0.5,
+      center: 1,
+      bounds: true,
+      boundsPadding: 1,
+      boundsDisabledForZoom: true,
+    });
+
+    document.querySelector('body')?.addEventListener('touchend', () => {
+      this.panzoom.pause();
+    });
+
+    this.zoom();
+    elem.parentElement.addEventListener('wheel', this.panzoom.zoomWithWheel);
+    elem.addEventListener('panzoomend', (event: any) => {
+      //   console.log(elementIsVisibleInViewport(this.world.nativeElement, true));
+      // if (!elementIsVisibleInViewport(this.world.nativeElement)) this.zoom();
+    });
+  }
+
   @Input() imageUrl: string = '';
   public title: string = '';
   @HostListener('document:keydown.escape', ['$event']) onKeydownHandler(
-    event: KeyboardEvent
+    event: KeyboardEvent,
   ) {
     this.closePhotoView();
   }
@@ -26,8 +78,10 @@ export class PhotoViewerComponent {
     this.title = this.photoViewer.title;
   }
   public scrollCheck: ReturnType<typeof setInterval> | null = null;
-  constructor(public photoViewer: PhotoViewerService) {}
+  constructor(public photoViewer: PhotoViewerService) { }
   ngAfterViewInit() {
+    this.initPanzoom();
+    ///
     this.scrollCheck = setInterval(() => {
       this.onScroll();
     }, 1000);
