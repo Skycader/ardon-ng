293a93e50285347892beb3c7d47dee2a54008a23
skycader <skycader@yandex.ru>
2024-03-06 02:34:00 +0300

    main: vk style grid

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environemnt = {
-  apiUrl: 'http://localhost:3000', //local NEST.JS APP
-  // apiUrl: 'api',
+  // apiUrl: 'http://localhost:3000', //local NEST.JS APP
+  apiUrl: 'api',
   // apiUrlNest: 'http://localhost:3000',
   // backendUrl: 'https://backend.ardon.vodri.com',
 };
