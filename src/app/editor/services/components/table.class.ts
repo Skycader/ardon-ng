6fc69b8e01c1d4f7109e34db30c3bf9354274053
skycader <skycader@yandex.ru>
2024-03-09 21:10:52 +0300

    main: better enum usage

--- src/app/editor/services/components/table.class.ts
+++ src/app/editor/services/components/table.class.ts
@@ -1,14 +1,11 @@
+import { EditBlockType } from '../../models/editBlockType.enum';
 import { EditBlockTableInterface } from '../../models/editorComponent.interface';
 
 export class EditBlockTable implements EditBlockTableInterface {
   title = 'Таблица';
-  type = TableType.table;
+  type = EditBlockType.table;
   icon = 'table_chat';
   content = {
     table: [],
   };
-}
-
-export enum TableType {
-  table = 'table',
 }
