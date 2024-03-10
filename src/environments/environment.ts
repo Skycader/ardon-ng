68fabb15e50d8a8c01dcc781dd5222d9edd1e2b1
skycader <skycader@yandex.ru>
2024-03-10 17:44:29 +0300

    main: would need to fix environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environemnt = {
   // apiurl: 'http://localhost:3000', //local nest.js app
-  apiurl: 'api',
+  apiUrl: 'api',
   // apiurlnest: 'http://localhost:3000',
   // backendurl: 'https://backend.ardon.vodri.com',
 };
