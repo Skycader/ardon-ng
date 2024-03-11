ea868307f28cd8667a03e2f9b9836e595c823aeb
skycader <skycader@yandex.ru>
2024-03-11 06:00:35 +0300

    gap

--- src/app/main/layouts/main-layout/main-layout.component.ts
+++ src/app/main/layouts/main-layout/main-layout.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ConfigService } from '../../../ardon-core/services/config.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'ardon-main-layout',
@@ -7,5 +8,16 @@ import { ConfigService } from '../../../ardon-core/services/config.service';
   styleUrl: './main-layout.component.scss',
 })
 export class MainLayoutComponent {
-  constructor(public configService: ConfigService) { }
+  public showContent: boolean = true;
+
+  constructor(
+    public configService: ConfigService,
+    private activatedRoute: ActivatedRoute,
+  ) { }
+
+  ngOnInit() {
+    this.activatedRoute.data.subscribe((response) => { });
+  }
+
+  ngAfterViewInit() { }
 }
--- src/app/main/utils/resolvers/main.resolver.ts
+++ src/app/main/utils/resolvers/main.resolver.ts
@@ -12,14 +12,14 @@ import { LoadingBarService } from '../../../ardon-common/services/loading-bar.se
   providedIn: 'root',
 })
 export class MainResolver implements Resolve<string> {
-  constructor(private loadingBarService: LoadingBarService) {}
+  constructor(private loadingBarService: LoadingBarService) { }
 
   resolve(route: ActivatedRouteSnapshot): Observable<string> {
     return of('123').pipe(
       tap(() => {
         this.loadingBarService.loading$.next(true);
       }),
-      delay(1000),
+      delay(0),
       switchMap(() => of('some profile data')),
       tap(() => {
         this.loadingBarService.loading$.next(false);
