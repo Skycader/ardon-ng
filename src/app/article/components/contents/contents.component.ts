36a851d60e1eea745c67fd786c8d0b97a3260a35
skycader <skycader@yandex.ru>
2024-03-12 01:29:32 +0300

    main: css tricks accordion

--- src/app/article/components/contents/contents.component.ts
+++ src/app/article/components/contents/contents.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { ArdonArticleBlockInterface } from '../../models/article.interface';
 
 export interface Section {
