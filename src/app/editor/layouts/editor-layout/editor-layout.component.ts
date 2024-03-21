1d789a6abc2739f3e8638bd2c845713e6d8e5f20
skycader <skycader@yandex.ru>
2024-03-21 13:55:45 +0300

    ok

--- src/app/editor/layouts/editor-layout/editor-layout.component.ts
+++ src/app/editor/layouts/editor-layout/editor-layout.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'ardon-editor-layout',
   templateUrl: './editor-layout.component.html',
-  styleUrl: './editor-layout.component.scss'
+  styleUrl: './editor-layout.component.scss',
 })
 export class EditorLayoutComponent {
+  constructor(private titleService: Title) { }
 
+  ngOnInit() {
+    this.titleService.setTitle('Редактор статей');
+  }
 }
--- src/app/profile/layouts/profile-layout/profile-layout.component.ts
+++ src/app/profile/layouts/profile-layout/profile-layout.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CardConfigInterface } from '../../../ardon-common/models/cardConfig.interface';
 import { ProfileComponentsClass } from '../../data/profile.components.class';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'ardon-profile-layout',
@@ -13,4 +14,9 @@ export class ProfileLayoutComponent {
   };
   public components: CardConfigInterface[] = new ProfileComponentsClass()
     .components;
+
+  constructor(private titleService: Title) { }
+  ngOnInit() {
+    this.titleService.setTitle('Личный кабинет');
+  }
 }
